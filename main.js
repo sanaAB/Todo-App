209c8e6b5ba1956fb444f88c9cf639d969289604
sanaAB <sanaabohelal@gmail.com>
2021-03-23 19:13:40 +0100

    modified edit function

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ form.addEventListener("submit", (event)=> {
 	ClearInputField();
 });
 
+
 itemsList.addEventListener("click", (event) => {
 	const clickedEl = event.target;
 	if (clickedEl.classList.contains("delete")) {
@@ -20,22 +21,43 @@ itemsList.addEventListener("click", (event) => {
 	}
 
 	else if (clickedEl.tagName.toLowerCase() === "p") {
-		console.log("you clicked edit");
 		clickedEl.onkeydown = (event) => {
 			if (event.key === "Enter") {
 				event.preventDefault(); // prevents line breaks
 				const newText = clickedEl.textContent;
-				console.log("cuuuuuuuuurrrr  "+newText);
 				if (clickedEl.textContent.trim().length < 2) {
 					return alert("Todo item cannot be empty or less then two chars.");
 				  }
 				  const clickedItemId = clickedEl.parentElement.parentElement.dataset.id;
 				  const currTodo = todos.find((todo) => clickedItemId === todo.id);
 				  currTodo.title = newText;
-				  currTodo.isEditable = false;
+				  //currTodo.isEditable = false;
 				  render();	
 				}
 			};
+	}
+	else if (clickedEl.tagName.toLowerCase() === "input"){
+		const checkedBox = clickedEl;
+		//checkedBox.checked =true;
+		console.log("you clicked now   "+ clickedEl);
+		const checkedBoxId = clickedEl.parentElement.parentElement.dataset.id;
+		console.log("The id of the clicked element is   "+ checkedBoxId);
+		const currTodo = todos.find((todo) => checkedBoxId === todo.id);
+		console.log("current todo is Not checked so it's editable " + currTodo.isEditable);
+				if(checkedBox.checked){
+					currTodo.isDone = true;
+					currTodo.isEditable = false;
+					console.log(currTodo);
+					//event.target.classList.toggle("checked");
+					//this.getElementById("p").isEditable = false;
+				}
+				else 
+				{
+					currTodo.isDone = false;
+					currTodo.isEditable = true;
+					console.log(currTodo);
+				}
+				render();
 	}
 });
 
@@ -60,10 +82,7 @@ function addItem() {
 
 
 function deleteButton(targetItem){
-	console.log(targetItem);
-	console.log(todos);
 	const clickedItemId =targetItem.parentElement.parentElement.dataset.id;
-	console.log(clickedItemId)
 	todos = todos.filter((todo, index) => {
 		return todo.id !== clickedItemId;
 	  });
@@ -89,19 +108,30 @@ function clearAllItems()
 	  }
 }
 
+const ClearButton = () =>{
+	clearAllItems();
+	todos = [];
+} 
+
+
 function render() {
 	clearAllItems();
 	todos.forEach((todo) => {
+		// if (todo.isDone=true)
+		//  {icontenteditable = false;}
+    	// else {icontenteditable = true;}
 		const template = `
 		<li data-id=${todo.id} class="li_style">
 		<div class='buttons'>
-		  <p contenteditable=${isEditable}>
-			<input type='checkbox' checkbox=${todo.isDone} />
-			${todo.title}
-		  </p>
+			<input type='checkbox' checkbox=${todo.isDone}  />
+		  	<p contenteditable=${todo.isEditable} id="p">
+			  ${todo.title}
+			</p>
+		</div>
+		<div>
 		  <button class="delete">delete</button>
 		  <button type="submit">edit</button>
-		  </div>
+		</div>
 		  </li>
 		`;
 		itemsList.insertAdjacentHTML("beforeend", template);
@@ -130,7 +160,23 @@ function render() {
 
 
 
+	// if(checkedBox.checked)
+	// {	
+	// 	console.log("inside if checked == true     "+currTodo.isDone);
+	// 	currTodo.parentElement.parentElement	.isEditable =false;
+	// 	currTodo.isDone = true;
+	// 	console.log(currTodo.isDone);
+	// }
 	
+
+			// }
+			// else {
+			// 	console.log(checkedBox.checked);
+
+			// 	currTodo.isDone =false;
+			// 	currTodo.isEditable = true;
+			// }
+
 
 
 
